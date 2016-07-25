7378d6c9f0712888b5dd60323d5b042fe23c262a
yamad <jyamada1@gmail.com>
2016-07-24 22:12:16 -0700

    allow shell commands as test input

--- tests-util.sh
+++ tests-util.sh
@@ -14,19 +14,25 @@ ensure()
     fi
 
     _ntests=$(($_ntests + 1))
-    echo "  test $_ntests: OK"
+    echo "  OK"
 }
 
 # pass name of binary to test. must have a file called
 # tests/<name>-tests.txt in which test inputs and expected outputs are
-# listed on subsequent lines
+# listed on subsequent lines. if the second argument is
+# "command-input", then the test input line is a shell command
 run-tests()
 {
     echo "Run tests for ${1}:"
     cat "tests/${1}-tests.txt" |
         while { read -r input; read -r expected; }
-    do
-        actual=`bin/${1} ${input}`
-        ensure "${actual}" "${expected}"
-    done
+        do
+            if [ "$2" == "command-input" ]
+            then
+                actual=$(eval ${input})
+            else
+                actual=`bin/${1} ${input}`
+            fi
+            ensure "${actual}" "${expected}"
+        done
 }
